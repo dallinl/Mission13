0053abefc56f7c08ade0d6ef12f00d000019182b
dallinl <dladle99@gmail.com>
2022-03-30 17:16:25 -0600

    Add project files.

---
+++ Components/TeamsViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Mission13.Models;
+
+namespace Mission13.Components
+{
+    public class TeamsViewComponent : ViewComponent
+    {
+        private BowlersDbContext repo { get; set; }
+
+        public TeamsViewComponent (BowlersDbContext temp)
+        {
+            repo = temp;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            // get the team name from routedata
+            ViewBag.SelectedTeam = RouteData?.Values["teamName"];
+
+            var teams = repo.Teams
+                .Select(x => x.TeamName)
+                .Distinct()
+                .OrderBy(x => x);
+
+            return View(teams); 
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Mission13.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission13.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BowlersDbContext _context { get; set;}
+        
+        public HomeController(BowlersDbContext temp)
+        {
+            _context = temp;
+        }
+        
+        // Home Page
+        [HttpGet]
+        public IActionResult Index(string teamName)
+        {
+            var bowlers = _context.Bowlers
+                .Include(x => x.Team)
+                .Where(t => t.Team.TeamName == teamName || teamName ==null)
+                .ToList();
+
+            return View(bowlers);
+        }
+
+
+        // CRUD Functionality
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            ViewBag.Teams = _context.Teams.ToList();
+            return View(); 
+        }
+
+        [HttpPost]
+        public IActionResult Create(Bowler b)
+        {
+            b.BowlerID = (_context.Bowlers.ToList().Capacity + 1);
+            _context.Add(b);
+            _context.SaveChanges();
+            return View("Confirmation");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int bowlerid)
+        {
+            ViewBag.Teams = _context.Teams.ToList();
+
+            var bowler = _context.Bowlers.Single(x => x.BowlerID == bowlerid);
+
+            return View("Create", bowler);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Bowler b)
+        {
+            _context.Update(b);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int bowlerid)
+        {
+            var b = _context.Bowlers.Single(x => x.BowlerID == bowlerid);
+
+            return View(b);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Bowler b)
+        {
+            _context.Bowlers.Remove(b);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+
+    }
+}
